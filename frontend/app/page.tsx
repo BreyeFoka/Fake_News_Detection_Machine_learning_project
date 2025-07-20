ac37186e6f5fb51f6d4f264f5590bd6339ff7131
Breye Foka <breyefokalagloire@gmail.com>
2025-07-21 00:07:52 +0100

    fix environment variables

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    const res = await fetch(process.env.NEXT_PUBLIC_API_URL , {
+    const res = await fetch(process.env.NEXT_PUBLIC_API_URL + "/predict", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ headline, text }),
